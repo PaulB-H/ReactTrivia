fbc120a9b540542aec308c3f59933c5141801d6e
Patrick Abromeit <patrick.abromeit@gmail.com>
2019-12-16 12:58:23 -0500

    heroku fix

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const socketIO = require('socket.io');
+const io = socketIO(server, { origin: "*:*" });
 
 const PORT = process.env.PORT || 8080;
 
